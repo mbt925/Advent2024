7dd61c0cad8174f1c638895285440d3c82554e04
Mohsen.Biglari <mohsen.biglari@Wolt.com>
2024-12-07 13:07:21 +0100

    Day 7

---
+++ src/Day07.kt
@@ -0,0 +1,67 @@
+import EquationOperator.*
+
+enum class EquationOperator {
+    Sum, Mul, Concat;
+
+    fun run(operand1: Long, operand2: Long) = when (this) {
+        Sum -> operand1 + operand2
+        Mul -> operand1 * operand2
+        Concat -> "$operand1$operand2".toLong()
+    }
+}
+
+fun main() {
+
+    data class Equation(
+        val result: Long,
+        val operands: List<Long>,
+        val validOperators: Set<EquationOperator>,
+    ) {
+        fun isPossible(operators: List<EquationOperator> = emptyList()): Boolean {
+            if (operators.size == operands.size - 1) {
+                val currentResult = operands.foldIndexed(0L) { index, acc, num ->
+                    when {
+                        index == 0 -> num
+                        else -> operators[index - 1].run(acc, num)
+                    }
+                }
+                return currentResult == result
+            }
+            validOperators.forEach {
+                if (isPossible(operators + it)) return true
+            }
+            return false
+        }
+    }
+
+    fun String.toEquation(validOperators: Set<EquationOperator>): Equation {
+        val split = this.split(':', ' ')
+        return Equation(
+            result = split[0].toLong(),
+            operands = split.subList(2, split.size).map { it.toLong() },
+            validOperators = validOperators,
+        )
+    }
+
+    fun part1(input: List<String>): Long {
+        return input.fold(0L) { acc, line ->
+            val equation = line.toEquation(setOf(Sum, Mul))
+            acc + if (equation.isPossible()) equation.result else 0
+        }
+    }
+
+    fun part2(input: List<String>): Long {
+        return input.fold(0L) { acc, line ->
+            val equation = line.toEquation(setOf(Sum, Mul, Concat))
+            acc + if (equation.isPossible()) equation.result else 0
+        }
+    }
+
+    val testInput1 = readInput("Day07_test")
+    check(part1(testInput1) == 3749L)
+    check(part2(testInput1) == 11387L)
+
+    val input = readInput("Day07")
+    part1(input).println()
+    part2(input).println()
+}
