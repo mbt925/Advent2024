b06c15e16426166694512fd6e7f49c933febae09
Mohsen.Biglari <mohsen.biglari@Wolt.com>
2024-12-22 21:33:45 +0100

    Day 22

---
+++ src/Day22.kt
@@ -0,0 +1,80 @@
+fun main() {
+
+    class SecretNumberManager {
+
+        fun next(current: Long, n: Int): Long {
+            var counter = 0
+            var secret = current
+            while (counter < n) {
+                secret = nextSecret(secret)
+                counter++
+            }
+            return secret
+        }
+
+        fun bestSeqPrice(secrets: List<Int>, n: Int): Int {
+            val mapOfSeqsWithMaxPrice = secrets.map { mapOfSeqsWithMaxPrice(it, n) }
+            val totalPriceMap = mutableMapOf<UInt, Int>()
+            mapOfSeqsWithMaxPrice.forEach {
+                it.entries.forEach { (seq, price) ->
+                    totalPriceMap[seq] = totalPriceMap.getOrDefault(seq, 0) + price
+                }
+            }
+            return totalPriceMap.maxOf { it.value }
+        }
+
+        private fun mapOfSeqsWithMaxPrice(current: Int, n: Int): Map<UInt, Int> {
+            var counter = 0
+            var secret = current.toLong()
+            var seq = 0u // 1 byte per diff. 4 bytes = Int
+            val map = mutableMapOf<UInt, Int>()
+            while (counter < n) {
+                val newSecret = nextSecret(secret)
+                val diff = (((newSecret % 10) - (secret % 10)) and 0xFF).toUInt() // 0xFF only keeps the lower byte
+                seq = seq shl 8
+                seq = seq or diff
+                if (counter >= 3) {
+                    map.putIfAbsent(seq, (newSecret % 10).toInt())
+                }
+                counter++
+                secret = newSecret
+            }
+            return map
+        }
+
+        private fun nextSecret(currentSecret: Long): Long {
+            return currentSecret
+                .mixWith { it shl 6 }
+                .prune()
+                .mixWith { it shr 5 }
+                .prune()
+                .mixWith { it shl 11 }
+                .prune()
+        }
+
+        fun Long.mixWith(other: (curr: Long) -> Long): Long = this xor other(this)
+        fun Long.prune() = this and 0xFFFFFF
+    }
+
+    fun part1(input: List<String>): Long {
+        val secretNumberManager = SecretNumberManager()
+        return input.fold(0L) { acc, secret ->
+            acc + secretNumberManager.next(secret.toLong(), 2000)
+        }
+    }
+
+    fun part2(input: List<String>): Int {
+        val secretNumberManager = SecretNumberManager()
+        val secrets = input.map { it.toInt() }
+        return secretNumberManager.bestSeqPrice(secrets, 2000)
+    }
+
+    val testInput11 = readInput("Day22_test")
+    check(part1(testInput11) == 37327623L)
+    val testInput12 = readInput("Day22_test2")
+    check(part2(testInput12) == 23)
+
+    val input = readInput("Day22")
+    part1(input).println()
+    part2(input).println()
+}
